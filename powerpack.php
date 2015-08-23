43dab6a713ec071e9ab52e8802a1365e1d7ce921
Ramon van Belzen <Ramoonus@users.noreply.github.com>
2015-08-23 11:12:09 +0200

    create header

--- powerpack.php
+++ powerpack.php
@@ -1,1 +1,11 @@
 <?php
+/*
+Plugin Name: Powerpack
+Plugin URI:  http://wordpress.org/extend/plugins/powerpack/
+Description: Adds a dozen of plugins in one go
+Version:     1.0-alpha
+Author:      Ramoonus
+Author URI:  http://www.ramoonus.nl/wordpress/powerpack/
+Text Domain: powerpack
+Domain Path: /language
+ */
