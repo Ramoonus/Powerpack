5c3df7d87ee5b1bb91b29b3f7ca5f91ebdf7d30d
Ramon van Belzen <github@ramoonus.nl>
2015-08-30 12:46:35 +0200

    include d3.js

--- powerpack.php
+++ powerpack.php
@@ -14,8 +14,22 @@ Domain Path: /language
  * Detect if WordPress is above 4.3
  * 
  * @todo: msg, disable self
+ * @version 4.3
+ * @since 1.0
  */
  if ( version_compare( $GLOBALS['wp_version'], '4.3', '<' ) ) { 
  	// todo
- } 
+ }
 
+/**
+ * D3.js
+ *
+ * @version 3.5.6
+ * @todo toggle minify
+ * @since 1.0
+ * @return void
+ */
+function rw_pp_d3js() {
+ wp_enqueue_script('d3.js', plugins_url( 'js/d3/d3.min.js', __FILE__ ) );
+}
+add_action('wp_enqueue_scripts', 'rw_pp_d3js');
