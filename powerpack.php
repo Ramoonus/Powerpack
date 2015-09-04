eeddafe16c66a8e1d4a58a5b81d00a47ff1049d6
Ramon van Belzen <github@ramoonus.nl>
2015-09-04 18:52:53 +0200

    add lt ie9 for selectivizr

--- powerpack.php
+++ powerpack.php
@@ -51,3 +51,4 @@ function rw_selectivizr() {
 	wp_enqueue_script('selectivizr',plugins_url( '/js/selectivizr.js' , __FILE__ ), array(), '1.0.b3' );
 }
 add_action('wp_enqueue_scripts', 'rw_selectivizr');
+wp_script_add_data( 'selectivizr', 'conditional', 'lt IE 9' ); /** new */
